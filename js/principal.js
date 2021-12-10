f7732b435dfe343b400aeefdf03c02a1481ffd85
Bruno Oliveira de Souza <bruno_odesouza@hotmail.com>
2021-12-10 17:21:00 -0300

    Alterando textos do banner

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,4 @@
-var banners =
-["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var banners = ["Do lixo ao luxo!","Reaproveitar é aproveitar!"];
 var bannerAtual = 0;
 function trocaBanner() {
 bannerAtual = (bannerAtual + 1) % 2;
